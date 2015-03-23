1ff136f178a76ab5732906b1d4667e7041b13b6c
mihuie <huie.michael@gmail.com>
2015-03-23 14:52:20 +0000

    part 4

--- js/resources.js
+++ js/resources.js
@@ -6,7 +6,9 @@ game.resources = [
 	{name: "gripe_run_right",     type:"image",	src: "data/img/sprite/gripe_run_right.png"},
 	// our level tileset
 	{name: "area01_level_tiles",  type:"image",	src: "data/img/map/area01_level_tiles.png"},
-	
+	// the parallax background
+	{name: "area01_bkg0",         type:"image",	src: "data/img/area01_bkg0.png"},
+	{name: "area01_bkg1",         type:"image",	src: "data/img/area01_bkg1.png"},
 	/* 
 	 * Maps. 
  	 */
@@ -14,8 +16,7 @@ game.resources = [
 
 	/* 
 	 * Background music. 
-	 */	
-	
+	 */		
 	/* 
 	 * Sound effects. 
 	 */
