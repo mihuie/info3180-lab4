44cb25988a6f08a8632cd67ea1972ebc1985a6ec
mihuie <huie.michael@gmail.com>
2015-03-23 13:36:08 +0000

    part 3

---
+++ js/resources.js
@@ -0,0 +1,22 @@
+game.resources = [
+	/**
+	 * Graphics.
+	 */
+  // the main player spritesheet
+	{name: "gripe_run_right",     type:"image",	src: "data/img/sprite/gripe_run_right.png"},
+	// our level tileset
+	{name: "area01_level_tiles",  type:"image",	src: "data/img/map/area01_level_tiles.png"},
+	
+	/* 
+	 * Maps. 
+ 	 */
+	{name: "area01",              type: "tmx",	src: "data/map/area01.tmx"}
+
+	/* 
+	 * Background music. 
+	 */	
+	
+	/* 
+	 * Sound effects. 
+	 */
+];
