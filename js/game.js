1c8fbe4503489a7f134a58fd4a7c822ed95033c3
mihuie <huie.michael@gmail.com>
2015-03-23 15:17:46 +0000

    part 4

--- js/game.js
+++ js/game.js
@@ -44,6 +44,8 @@ var game = {
 
     // register our player entity in the object pool
     me.pool.register("mainPlayer", game.PlayerEntity);
+    me.pool.register("CoinEntity", game.CoinEntity);
+//     me.pool.register("EnemyEntity", game.EnemyEntity);
 
     // enable the keyboard
     me.input.bindKey(me.input.KEY.LEFT,  "left");
--- js/resources.js
+++ js/resources.js
@@ -6,6 +6,8 @@ game.resources = [
 	{name: "gripe_run_right",     type:"image",	src: "data/img/sprite/gripe_run_right.png"},
 	// our level tileset
 	{name: "area01_level_tiles",  type:"image",	src: "data/img/map/area01_level_tiles.png"},
+  // the spinning coin spritesheet
+	{name: "spinning_coin_gold",  type:"image",	src: "data/img/sprite/spinning_coin_gold.png"},
 	// the parallax background
 	{name: "area01_bkg0",         type:"image",	src: "data/img/area01_bkg0.png"},
 	{name: "area01_bkg1",         type:"image",	src: "data/img/area01_bkg1.png"},
